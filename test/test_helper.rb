2dff004648fe7d7eb43f6e263143ffb2dca5d9a9
Matijs van Zuijlen <matijs@matijs.net>
2014-10-18 14:27:45 +0200

    Fix formatting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,10 @@ require 'test/unit'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'iba'
 
-class Test::Unit::TestCase 
+class Test::Unit::TestCase
   include Iba::BlockAssertion
 
   def combinator &blk
     return Iba::Combinator.new(&blk)
   end
-
 end

7b5a764361051d016fc9bbdf0fbac31084ecb9b6
Matijs van Zuijlen <matijs@matijs.net>
2014-10-19 11:31:51 +0200

    Correct AlignParameters offenses

--- test/analyse_test.rb
+++ test/analyse_test.rb
@@ -14,20 +14,20 @@ class AnalyseTest < Test::Unit::TestCase
   def test_operator_equals
     foo = 42
     bar = 23
-    assert_equal "(foo == bar) is false\nfoo is 42, bar is 23",
-      combinator { foo == bar }.analyse
+    result = combinator { foo == bar }.analyse
+    assert_equal "(foo == bar) is false\nfoo is 42, bar is 23", result
   end
 
   def test_operator_equals_literal
     foo = 42
-    assert_equal "(foo == 23) is false\nfoo is 42",
-      combinator { foo == 23 }.analyse
+    result = combinator { foo == 23 }.analyse
+    assert_equal "(foo == 23) is false\nfoo is 42", result
   end
 
   def test_operator_equals_array_literal
     foo = [1, 'bar']
-    assert_equal "(foo == [2, \"baz\"]) is false\nfoo is [1, \"bar\"]",
-      combinator { foo == [2, 'baz'] }.analyse
+    result = combinator { foo == [2, 'baz'] }.analyse
+    assert_equal "(foo == [2, \"baz\"]) is false\nfoo is [1, \"bar\"]", result
   end
 
   def test_string_variable

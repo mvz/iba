10c81d37887bb8791dcb43e6a42a9b874e9d9363
Matijs van Zuijlen <matijs@matijs.net>
2016-12-16 20:16:39 +0100

    Clean up requires in tests

--- test/display_test.rb
+++ test/display_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path('test_helper.rb', File.dirname(__FILE__))
+require_relative 'test_helper'
 
 # Test how the combinator displays the parsed block contents.
 class DisplayTest < Test::Unit::TestCase
